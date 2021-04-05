8d0bd88e0bfe3d734b6a4db5d4fffc5a2198c382
Hugh Winchester <hugh.winchester@gmail.com>
2021-04-05 07:03:28 +0100

    Code formatting

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import './App.css';
-import About from './components/About'
-import Contact from './components/Contact'
-import Landing from './components/Landing'
-import { useState } from 'react';
-import { Element, scroller, } from "react-scroll";
-
-
-
-
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Landing from "./components/Landing";
+import { useState } from "react";
+import { Element, scroller } from "react-scroll";
 
 function App() {
-
-  const [showAbout, setShowAbout] = useState(false)
-  const [showContact, setShowContact] = useState(false)
+  const [showAbout, setShowAbout] = useState(false);
+  const [showContact, setShowContact] = useState(false);
 
   const setAboutClick = () => {
-    console.log("fire!")
-    setShowAbout(true)
+    console.log("fire!");
+    setShowAbout(true);
     scroller.scrollTo("aboutElement", {
       duration: 2000,
       delay: 100,
-      smooth: true, 
+      smooth: true,
       offset: 50,
-    })
-  }
+    });
+  };
 
   const setContactClick = () => {
-    console.log("fire!")
-    setShowContact(true)
+    console.log("fire!");
+    setShowContact(true);
     scroller.scrollTo("contactElement", {
       duration: 2000,
       delay: 100,
-      smooth: true, 
+      smooth: true,
       offset: 50,
-    })
-  }
-
+    });
+  };
 
   return (
     <div>
-      <Landing setAbout={setAboutClick} showArrow={showAbout}/>
-      {showAbout ? <About setContact={setContactClick} showArrow={showContact}/> : null}
-      <Element name="aboutElement"/>
-      {showContact ? <Contact/> : null}
-      <Element name="contactElement"/>
+      <Landing setAbout={setAboutClick} showArrow={showAbout} />
+      {showAbout ? (
+        <About setContact={setContactClick} showArrow={showContact} />
+      ) : null}
+      <Element name="aboutElement" />
+      {showContact ? <Contact /> : null}
+      <Element name="contactElement" />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,25 @@
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Contact = () => {
-
-    return( 
-<div>
-    <div class="text-center">
-        <h1 class="header" id="contact">Contact</h1>
+  return (
+    <div>
+      <div class="text-center">
+        <h1 class="header" id="contact">
+          Contact
+        </h1>
+      </div>
+      <footer class="social text-center">
+        <a href="https://github.com/hughwin">
+          <FontAwesomeIcon icon={faGithub} size="6x" />
+        </a>
+        <a href="https://uk.linkedin.com/in/hughwinchester">
+          <FontAwesomeIcon icon={faLinkedin} size="6x" />
+        </a>
+      </footer>
     </div>
-<footer class="social text-center">
-    <a href="https://github.com/hughwin">
-    <FontAwesomeIcon icon={faGithub} size="6x"/>
-    </a>
-    <a href="https://uk.linkedin.com/in/hughwinchester">
-    <FontAwesomeIcon icon={faLinkedin} size="6x"/>
-    </a>
-</footer>
-</div>
-    )
-}
+  );
+};
 
-    export default Contact
+export default Contact;
