9629a2ad03ddfde50a861028d00ac1af62b9b7a4
Hugh Winchester <hugh.winchester@gmail.com>
2021-04-05 06:57:14 +0100

    Added Helmet to Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,22 +1,36 @@
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Helmet } from "react-helmet";
 
 const Landing = ({ setAbout, showArrow }) => {
+  const handleAboutClick = () => {
+    setAbout();
+  };
 
-    const handleAboutClick = () => {
-        setAbout()
-      }
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Hugh Winchester</title>
+        <link rel="canonical" href="http://hughwinchester.co.uk" />
+      </Helmet>
+      <div class="top container text-center">
+        <h1 class="title" id="name">
+          Hugh Winchester
+        </h1>
+        {!showArrow ? (
+          <FontAwesomeIcon
+            icon={faArrowDown}
+            onClick={() => handleAboutClick()}
+            size="3x"
+            class="fadeIn"
+          />
+        ) : (
+          <FontAwesomeIcon icon={faArrowDown} size="3x" class="fadeOut" />
+        )}
+      </div>
+    </div>
+  );
+};
 
-return(
-        <div class="top container text-center">
-            <h1 class="title" id="name">Hugh Winchester</h1>
-            {!showArrow ?
-            <FontAwesomeIcon icon={faArrowDown} onClick={()=>handleAboutClick()} size="3x" class="fadeIn"/> :
-            <FontAwesomeIcon icon={faArrowDown} size="3x" class="fadeOut"/>}
-        </div>
-)
-}
-
-
-export default Landing
+export default Landing;
