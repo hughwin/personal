fa8e097c1b5b13cb0a055d6ac53a82d3e66014cf
Hugh Winchester <hugh.winchester@gmail.com>
2021-11-09 10:08:52 +0000

    Updated Tinkering

--- src/components/Tinkering.js
+++ src/components/Tinkering.js
@@ -10,6 +10,13 @@ const Tinkering = ({ setContact, showArrow }) => {
     <div class="container text-center">
       <h1 class="header" id="tinkering">
         Tinkering
+        <ul>
+          <li><a href="https://github.com/hughwin/APIT_assessement">Java Blackjack with networking and concurrency</a></li>
+          <li><a href="https://github.com/BelfDev/uofg-msc-team-project">Top Trumps written in Java which stores scores to a database</a></li>
+          <li><a href="https://botsin.space/web/accounts/282572">NASA picture bot written using Python. Hosted on Heroku</a></li>
+          <li><a href="https://github.com/big-dill/macro-mate">MacroMate a wellness Django application</a></li>
+          <li><a href="https://hugh-calculator.herokuapp.com/">Simple calculator built in React.</a></li>
+        </ul>
       </h1>
       {!showArrow ? (
         <FontAwesomeIcon
